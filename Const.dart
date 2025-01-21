6cb66201dffec7fe00c0fd5148d7f4ad17be142e
Divyanshi Maurya <116268950+Jiya003@users.noreply.github.com>
2025-01-21 13:32:20 +0530

    Create Const.dart

---
+++ Const.dart
@@ -0,0 +1,19 @@
+void main() {
+  //Const Keyword
+  
+  const var1="Hello world!!";
+  const int var2=10;
+  const int var3;
+  print(var1);
+  print(var2);
+  
+  
+}
+/*
+Output:
+compileDDC
+main.dart:6:13: Error: The const variable 'var3' must be initialized.
+Try adding an initializer ('= expression') to the declaration.
+  const int var3;
+            ^^^^
+*/
