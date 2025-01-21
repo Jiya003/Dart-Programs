c7cf3507fe53b40207c6c57d9fa9265bab7a6bb2
Divyanshi Maurya <116268950+Jiya003@users.noreply.github.com>
2025-01-21 12:58:36 +0530

    Create TypeSafe.dart

---
+++ TypeSafe.dart
@@ -0,0 +1,19 @@
+void main() {
+  //Variable declaration
+  
+  int a=5;
+  int b=6;
+  print(a+b);
+  
+  double c=3.17;
+  int d=4;
+  print(c+d);//int converted into double
+  
+  String str1="Dart is Easy";
+  String str2="Dart is Type Safe";
+  
+  print(str1+str2);
+  /*
+   * Type Safe*/
+  //print(a+str1);
+}
