8f43d4caaca0908fe95c754acb7ca49e936c691e
Divyanshi Maurya <116268950+Jiya003@users.noreply.github.com>
2025-01-21 21:07:47 +0530

    Create Output.dart

---
+++ Output.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+void main(){
+  print("Enter fav number: ");
+  int? n=int.parse(stdin.readLineSync()!);
+  print("Your fav number is $n");
+  stdout.write("Divyanshi Maurya");
+}
