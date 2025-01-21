59b5346698281f0622ced5f3efcb9c076e40bb19
Divyanshi Maurya <116268950+Jiya003@users.noreply.github.com>
2025-01-21 20:44:14 +0530

    Create ArithmeticOperators.dart

---
+++ ArithmeticOperators.dart
@@ -0,0 +1,42 @@
+void main() {
+ //Arithmetic Operators
+  
+  //Addition
+  int add1=5;
+  int add2=6;
+  print(add1+add2);
+  
+  //Subtraction
+  int sub1=7;
+  int sub2=4;
+  print(sub2-sub1);
+  print(sub1-sub2);
+  
+  //Unary Minus
+  int res1= -add1;
+  print(res1);
+  
+  //Multiply
+  int mul1=4;
+  double mul2=11;
+  double res2=mul1*mul2;
+  print("Multiplication of $mul1 and $mul2 is: $res2");
+  
+  //Division
+  double div=sub2/mul2;
+  print("Division of $sub2 and $mul2 is: $div");
+  
+  //  ~/ -> Quotient
+  int quo=sub2 ~/ mul2;
+  print("Quotient of $sub2 and $mul2 is: $quo");
+  
+  //Remainder -> %
+  int rem= sub2%mul1;
+  print("Remainder of $sub1 and $mul2 is: $rem");
+  
+  
+  
+  
+  
+  
+}
